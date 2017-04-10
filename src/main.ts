ad489cffe57b57a8dcaf06b82d68627b4c7daa22
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-04-10 19:00:00 +0900

    fix(shader): Enahanced default gltf shader

--- src/main.ts
+++ src/main.ts
@@ -6,30 +6,32 @@ import MaterialFactory from "grimoirejs-fundamental/ref/Material/MaterialFactory
 import UniformResolverRegistry from "grimoirejs-fundamental/ref/Material/UniformResolverRegistry";
 import gltfUnlit from "raw-loader!./Shaders/gltf-unlit.sort";
 export default () => {
-  GrimoireInterface.register(
-    async () => {
-      GrimoireInterface.registerComponent("GLTFModel", GLTFModelComponent);
-      GrimoireInterface.registerComponent("GLTFAnimation", GLTFAnimationComponent);
-      GrimoireInterface.registerComponent("GLTFJoint", GLTFJointComponent);
-      GrimoireInterface.registerNode("model", ["GLTFModel"], {}, "object");
-      GrimoireInterface.registerNode("gltf-mesh", [], {
-        material: "new(gltf-unlit)"
-      }, "mesh");
-      GrimoireInterface.registerNode("gltf-joint", ["GLTFJoint"], {}, "object");
-      GrimoireInterface.registerNode("gltf-assets", [], {});
-      GrimoireInterface.registerNode("gltf-animation", ["GLTFAnimation"], {});
-      MaterialFactory.addSORTMaterial("gltf-unlit", gltfUnlit);
-      UniformResolverRegistry.add("JOINTMATRIX", (valInfo , material) => {
-        material.addArgument("boneMatrices", {
-          converter: "Object",
-          default: null
-        });
-        return (proxy, info) => {
-          if(info.renderable.renderArgs["gltf-boneMatrices"]){
-            proxy.uniformMatrixArray(valInfo.name, info.renderable.renderArgs["gltf-boneMatrices"]);
-          }
-        };
-      });
-    }
-  );
+    GrimoireInterface.register(
+        async () => {
+            GrimoireInterface.registerComponent("GLTFModel", GLTFModelComponent);
+            GrimoireInterface.registerComponent("GLTFAnimation", GLTFAnimationComponent);
+            GrimoireInterface.registerComponent("GLTFJoint", GLTFJointComponent);
+            GrimoireInterface.registerNode("model", ["GLTFModel"], {}, "object");
+            GrimoireInterface.registerNode("gltf-mesh", [], {
+                material: "new(gltf-unlit)"
+            }, "mesh");
+            GrimoireInterface.registerNode("gltf-joint", ["GLTFJoint"], {}, "object");
+            GrimoireInterface.registerNode("gltf-assets", [], {});
+            GrimoireInterface.registerNode("gltf-animation", ["GLTFAnimation"], {});
+            MaterialFactory.addSORTMaterial("gltf-unlit", gltfUnlit);
+            UniformResolverRegistry.add("JOINTMATRIX", (valInfo, material) => {
+                return (proxy, info) => {
+                    if (info.renderable.renderArgs["gltf-boneMatrices"]) {
+                        proxy.uniformMatrixArray(valInfo.name, info.renderable.renderArgs["gltf-boneMatrices"]);
+                    }
+                };
+            });
+            UniformResolverRegistry.add("AMBIENT_COEFFICIENT", (valInfo, material) => {
+                return (proxy, info) => {
+                    const amb = info.sceneDescription["ambientCoefficient"];
+                    proxy.uniformFloat(valInfo.name, amb || 0.1)
+                };
+            });
+        }
+    );
 };
