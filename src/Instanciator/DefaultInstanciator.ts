06453b54f5cb13a10a1cf9c7f62063565f511897
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-05-22 00:36:03 +0900

    fix: support for matrix property

--- src/Instanciator/DefaultInstanciator.ts
+++ src/Instanciator/DefaultInstanciator.ts
@@ -1,9 +1,11 @@
 import InstanciationRecipe from "../Parser/InstanciationRecipe";
 import GomlNode from "grimoirejs/ref/Node/GomlNode";
 import GLTFNode from "../Parser/Schema/GLTFNode";
+import Transform from "grimoirejs-fundamental/ref/Components/TransformComponent";
 
 import Quaternion from "grimoirejs-math/ref/Quaternion";
 import Vector3 from "grimoirejs-math/ref/Vector3";
+import Matrix from "grimoirejs-math/ref/Matrix";
 
 export default class DefaultInstanciator {
     public instanciateAll(recipe: InstanciationRecipe, node: GomlNode, scene: string | number): void {
@@ -32,16 +34,24 @@ export default class DefaultInstanciator {
         let currentNode;
         if (node.mesh !== void 0) {
             const primitives = recipe.primitives[node.mesh];
-            if (primitives.length === 1) {
-              const meshInfo = recipe.tf.meshes[node.mesh];
+            const meshInfo = recipe.tf.meshes[node.mesh];
+            if (primitives.length === 1) { // If the node contains single mesh
               const mat = recipe.materials[meshInfo.primitives[0].material];
-                const meshNode = parent.addChildByName("mesh", {
-                    geometry: primitives[0],
-                    material:mat
-                });
-                currentNode = meshNode;
-            } else {
-                console.error("multiple mesh is not supported yet")
+              const meshNode = parent.addChildByName("mesh", {
+                  geometry: primitives[0],
+                  material:mat
+              });
+              currentNode = meshNode;
+            } else { // If the node contains multiple mesh
+                const objectNode = parent.addChildByName("object",{});
+                for(let i = 0; i < primitives.length; i++){
+                  const mat = recipe.materials[meshInfo.primitives[i].material];
+                  const meshNode = objectNode.addChildByName("mesh", {
+                      geometry: primitives[i],
+                      material:mat
+                  });
+                }
+                currentNode = objectNode;
             }
         } else {
             currentNode = parent.addChildByName("object", {});
@@ -55,14 +65,22 @@ export default class DefaultInstanciator {
     }
 
     protected __applyTransform(node:GomlNode,nodeInfo:GLTFNode){
+      const transform = node.getComponent(Transform);
       if(nodeInfo.rotation){
-        node.setAttribute("rotation",new Quaternion([].concat(nodeInfo.rotation)));
+        transform.setAttribute("rotation",new Quaternion([].concat(nodeInfo.rotation)));
       }
       if(nodeInfo.translation){
-        node.setAttribute("position",new Vector3([].concat(nodeInfo.translation)));
+        transform.setAttribute("position",new Vector3([].concat(nodeInfo.translation)));
       }
       if(nodeInfo.scale){
-        node.setAttribute("scale",new Vector3([].concat(nodeInfo.scale)));
+        transform.setAttribute("scale",new Vector3([].concat(nodeInfo.scale)));
+      }
+      if(nodeInfo.matrix){
+        if(nodeInfo.rotation || nodeInfo.translation || nodeInfo.scale){
+          throw new Error("Matrix property can not be existed with other transoform property");
+        }
+        let mat = new Matrix(nodeInfo.matrix);
+        transform.applyMatrix(mat);
       }
     }
 }
