32cc5b0fc1a76e3e8fe474a11f739951386ddf04
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-10-16 00:06:39 +0900

    fix: Removed gltf-unlit shader

--- src/main.ts
+++ src/main.ts
@@ -9,23 +9,14 @@ import ImportResolver from "grimoirejs-fundamental/ref/Sort/ImportResolver";
 import GLExtRequestor from "grimoirejs-fundamental/ref/Resource/GLExtRequestor";
 import GLTFVertexMorpher from "./Components/GLTFVertexMorpher";
 export default () => {
-    if (typeof ImportResolver.staticImports["forward-shading"] !== "string") {
-        ImportResolver.staticImports["forward-shading"] = "";
-    }
     GLExtRequestor.request("OES_standard_derivatives")
     GLExtRequestor.request("OES_element_index_uint")
     GrimoireInterface.register(
         async () => {
             GrimoireInterface.registerComponent("GLTFModel", GLTFModelComponent);
             GrimoireInterface.registerComponent("GLTFJoint", GLTFJointComponent);
-            GrimoireInterface.registerComponent("GLTFVertexMorpher",GLTFVertexMorpher);
+            GrimoireInterface.registerComponent("GLTFVertexMorpher", GLTFVertexMorpher);
             GrimoireInterface.registerNode("model", ["GLTFModel"], {}, "object");
-            GrimoireInterface.registerNode("gltf-mesh", [], {
-                material: "new(gltf-unlit)"
-            }, "mesh");
-            GrimoireInterface.registerNode("gltf-joint", ["GLTFJoint"], {}, "object");
-            GrimoireInterface.registerNode("gltf-assets", [], {});
-            MaterialFactory.addSORTMaterial("gltf-unlit", gltfUnlit);
             MaterialFactory.addSORTMaterial("gltf-pbr-metallic-roughness", gltfPBRMetallicRoughness);
             UniformResolverRegistry.add("JOINTMATRIX", (valInfo, material) => {
                 return (proxy, info) => {
