3b2b3b8d4246dc1d5c9f98fe735c8f946ef62a60
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-11-04 21:09:06 +0900

    fix: CI script

--- shell/e2e.sh
+++ shell/e2e.sh
@@ -1,1 +1,1 @@
-curl -X POST -d "repositoryURL=$CIRCLE_REPOSITORY_URL" -d "currentBranch=$CIRCLE_BRANCH" -d "currentBuildNumber=$CIRCLE_BUILD_NUM" -d "previousBuildNumber=$CIRCLE_PREVIOUS_BUILD_NUM" -d "sha1=$CIRCLE_SHA1" $E2E_TRIGGER
+curl -X POST -d "repositoryURL=$CIRCLE_REPOSITORY_URL" -d "currentBranch=$CIRCLE_BRANCH" -d "currentBuildNumber=$CIRCLE_BUILD_NUM" -d "previousBuildNumber=$CIRCLE_PREVIOUS_BUILD_NUM" -d "sha1=$CIRCLE_SHA1" -d "pullRequest=$CI_PULL_REQUEST" $E2E_TRIGGER
