838891d821ccec2bf2ab9f901ecff3d4d44a0893
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-11-04 15:10:37 +0900

    fix: e2e curl command

--- shell/e2e.sh
+++ shell/e2e.sh
@@ -1,1 +1,1 @@
-curl -X POST -d "repositoryURL=$CIRCLE_REPOSITORY_URL&currentBranch=$CIRCLE_BRANCH&currentBuildNumber=$CIRCLE_BUILD_NUM&previousBuildNumber=$CIRCLE_PREVIOUS_BUILD_NUM&sha1=$CIRCLE_SHA1" $E2E_TRIGGER
+curl -X POST -d "repositoryURL=$CIRCLE_REPOSITORY_URL" -d "currentBranch=$CIRCLE_BRANCH" -d "currentBuildNumber=$CIRCLE_BUILD_NUM" -d "previousBuildNumber=$CIRCLE_PREVIOUS_BUILD_NUM" -d "sha1=$CIRCLE_SHA1" $E2E_TRIGGER
