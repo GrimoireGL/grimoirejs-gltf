99f8258737f25ff8c18b6ed9c48c20ebe8507a34
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-11-05 14:56:16 +0900

    fix: add tagging

--- shell/e2e.sh
+++ shell/e2e.sh
@@ -1,2 +1,3 @@
 aws s3 cp register/ s3://$S3_BUCKET_URL/js/$CIRCLE_SHA1 --recursive
+aws s3api put-object-tagging --bucket $S3_BUCKET_URL --key js/$CIRCLE_SHA1 --tagging TagSet=[{Key=library,Value=$CIRCLE_REPOSITORY_URL}]
 curl -X POST -d "repositoryURL=$CIRCLE_REPOSITORY_URL" -d "currentBranch=$CIRCLE_BRANCH" -d "currentBuildNumber=$CIRCLE_BUILD_NUM" -d "previousBuildNumber=$CIRCLE_PREVIOUS_BUILD_NUM" -d "sha1=$CIRCLE_SHA1" -d "pullRequest=$CI_PULL_REQUEST" $E2E_TRIGGER
