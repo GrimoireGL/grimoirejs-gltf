9f50d6eebb5c270d243386f5ec8c13bc6ea886ab
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-11-07 14:15:27 +0900

    fix: made __applyPBRMetallicRoughnessParameters protected

--- src/Parser/MaterialInstanciator/DefaultPBRMetallicRoughnessInstanciator.ts
+++ src/Parser/MaterialInstanciator/DefaultPBRMetallicRoughnessInstanciator.ts
@@ -22,12 +22,42 @@ export default class DefaultPBRMetallicRoughnessInstanciator implements IGLTFMat
     }
 
 
-    private __applyPBRMetallicRoughnessParameters(materialData: GLTFMaterial, pass: Pass, textures: { [key: string]: Texture2D; }) {
+    protected __applyPBRMetallicRoughnessParameters(materialData: GLTFMaterial, pass: Pass, textures: { [key: string]: Texture2D; }) {
         const pmr = materialData["pbrMetallicRoughness"];
         if (pmr.baseColorFactor) {
             pass.setArgument("baseColorFactor", pmr.baseColorFactor, null);
         }
         if (pmr.baseColorTexture) {
+            pass.setArgument("baseColorTexture", new TextureReference(textures[pmr.baseColorTexture.index]), null);
+        }
+        if (pmr.metallicFactor) {
+            pass.setArgument("metallicFactor", pmr.metallicFactor, null);
+        }
+        // TODO Remove? metallicTexture and roughnessTexture was removed from specification?
+        if ((pmr as any).metallicTexture) {
+            pass.setArgument("metallicTexture", new TextureReference(textures[(pmr as any).metallicTexture.index]), null);
+        }
+        if ((pmr as any).roughnessTexture) {
+            pass.setArgument("roughnessTexture", new TextureReference(textures[(pmr as any).roughnessTexture.index]), null);
+        }
+        if (pmr.roughnessFactor) {
+            pass.setArgument("roughnessFactor", pmr.roughnessFactor, null);
+        }
+        if (pmr.metallicRoughnessTexture) {
+            pass.setArgument("metallicRoughnessTexture", new TextureReference(textures[pmr.metallicRoughnessTexture.index]), null);
+        }
+        if (materialData["emissiveFactor"]) {
+            pass.setArgument("emissiveFactor", materialData.emissiveFactor, null);
+        }
+        if (materialData["emissiveTexture"]) {
+            pass.setArgument("emissiveTexture", new TextureReference(textures[materialData.emissiveTexture.index]), null);
+        }
+        if (materialData["normalTexture"]) {
+            pass.setArgument("normalTexture", new TextureReference(textures[materialData.normalTexture.index]), null);
+        }
+        if (materialData["occlusionTexture"]) {
+            pass.setArgument("occlusionTexture", new TextureReference(textures[materialData.occlusionTexture.index]), null);
+        } if (pmr.baseColorTexture) {
             pass.setArgument("baseColorTexture", new TextureReference(textures[pmr.baseColorTexture.index]), null);
         }
         if (pmr.metallicFactor) {
